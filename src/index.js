fd90479626523491e964406545e268d67bc3421a
Tanya92 <tatiana_mironchik@tut.by>
2019-03-17 23:18:59 +0300

    feat: create function getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
+function primeNumbers(maxNumber) {
+  const isPrime = new Array (maxNumber + 1).fill(true);
+  isPrime[0] = false;
+  isPrime[1] = false;
+
+  const primes = [];
+
+  for (let number = 2; number <= maxNumber; number ++){
+      if (isPrime[number] === true){
+          primes.push(number);
+      
+          let nextNumber = number * number;
+
+          while(nextNumber <= maxNumber) {
+              isPrime[nextNumber] = false;
+              nextNumber += number;
+          }
+      }
+  }
+  return primes;
+}
+
+
+function basePrimeDev(base) {
+  const primes = primeNumbers(base);
+  const primesCount = {};
+  for (let i = 0; i <= primes.length; i++) {
+      let dividend = base;
+      let divisor = primes[i];
+      primesCount[divisor] = 0;
+      while (dividend > 1) {
+          if (dividend % divisor === 0) {
+              primesCount[divisor]++;
+              dividend = dividend / divisor; 
+          } else {
+              break;
+          } 
+      }
+      if (primesCount[divisor] === 0) {
+          delete primesCount[divisor];
+      }
+  }
+  return primesCount;
+}
+
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  const primesBaseCount = basePrimeDev(base);
+  let numberCount = {};
+    for (let i in primesBaseCount) {
+      numberCount[i] = 0;
+      let pow = 1;
+      while (number > Math.pow(i, pow)) {
+        numberCount[i] += Math.floor(number / Math.pow(i, pow));
+        pow++;
+      }
+    }
+  
+  let countZeroes = Number.MAX_SAFE_INTEGER;
+  for (let i in primesBaseCount) {
+      let result = Math.floor(numberCount[i] / primesBaseCount[i]);
+      countZeroes = Math.min(countZeroes, result);
+  }
+  return countZeroes;
 }
